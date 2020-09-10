b173357dd50a090839d94f7f3d192f6377cfdf73
Yeongho Kim <yeonghoey@gmail.com>
2020-09-10 13:58:38 +0900

    Add Playables

---
+++ URP2D/Assets/Scenes/Playables/Play.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class Play : MonoBehaviour
+{
+    public Animator animator;
+
+    void Start()
+    {
+        PlayableGraph graph;
+        var clip = MakeClip();
+        var playable = AnimationPlayableUtilities.PlayClip(animator, clip, out graph);
+    }
+
+    AnimationClip MakeClip()
+    {
+        var clip = new AnimationClip();
+        clip.frameRate = 7;
+        return clip;
+    }
+}

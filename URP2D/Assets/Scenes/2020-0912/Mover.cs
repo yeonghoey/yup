4b3139d1745ae130e81394326d2ee31a3b6450ae
Yeongho Kim <yeonghoey@gmail.com>
2020-09-12 09:52:44 +0900

    Update

---
+++ URP2D/Assets/Scenes/2020-0912/Mover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+        var animator = GetComponent<Animator>();
+        animator.rootPosition = animator.rootPosition + new Vector3(h, v);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GetComponent<SpriteRenderer>().color = Color.black;
+        }
+    }
+}
